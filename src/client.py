c487a816efb8d1559ea983acfe5757354573dae4
formando <formando@formando.com>
2022-07-29 19:34:56 +0100

    New git folder + client.py add string docopt to _main function

--- src/client.py
+++ src/client.py
@@ -6,14 +6,28 @@ This client accepts the following options:
     $ python3 client.py (get|put) [-p serv_port] server source_file [dest_file] 
     $ python3 client.py [-p serv_port] server
 
-(C) João Galamba, 2022
+(C) Ana, Mendes 2022
 """
 
+from docopt import docopt
 
 def _main():
-    """
-    Implements the console based client.
-    """
+    '''
+Usage: 
+	client.py -h
+	client.py <option> [-p serv_port] server remote_file [local_file]
+
+Examples:
+	client.py get [-p serv_port] server remote_file [local_file]
+	client.py put [-p serv_port] server local_file [remote_file]		
+
+Options:
+	-h --help   Show information about the commands
+	-g --get    Get a file from server and save it as local_file
+	-p --put    Send a file to server and store it as remote_file
+	-d --dir    Obtain a listing of remote files. Optional command to be use.
+	-q --quit   Exit TFTP client
+    '''
 #:
 
 if __name__ == '__main__':
